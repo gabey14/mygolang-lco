bd8835693edbe137e0e75b0d3ac49cd73fde795c
gabey14 <abeygeorge14@gmail.com>
2021-10-08 18:02:13 +0530

    Video 25 completed

--- 22bitmorejson/main.go
+++ 22bitmorejson/main.go
@@ -17,7 +17,8 @@ type course struct {
 
 func main() {
 	fmt.Println("Welcome to json video")
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 
 }
 
@@ -38,4 +39,40 @@ func EncodeJson() {
 
 	fmt.Printf("%s\n", finalJson)
 
+}
+
+func DecodeJson() {
+
+	jsonDataFromWeb := []byte(`
+		{
+                "coursename": "React Js Bootcamp",
+                "price": 299,
+                "website": "LearnCodeOnline.in",
+                "tags": [ "web-dev","js"]
+        }
+	`)
+
+	var lcoCourse course
+
+	checkValid := json.Valid(jsonDataFromWeb)
+
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("JSON WAS NOT VALID")
+	}
+
+	// Some cases where you want to add data to key value pair
+
+	var myOnlineData map[string]interface{}
+
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for key, val := range myOnlineData {
+		fmt.Printf("Key is %v and value is %v and Type is %T\n", key, val, val)
+	}
 }
